e04138889ad9d8a126cf6f5e29c767c539586379
Goldobin Anton <722451@it-atlantic.com>
2019-04-14 22:38:05 +0300

    images, unix-time

--- js/custom.js
+++ js/custom.js
@@ -248,8 +248,67 @@ jQuery(document).ready(function($){
     initMap();
   };
 
+    function time(){
+        const unixTime = parseInt(new Date().getTime()/1000)
+        $('#unix-time').text(unixTime)
+        setTimeout( time,1000);
+    }
+    time()
 
+    document.getElementById("copyButton").addEventListener("click", function() {
+        copyToClipboard(document.getElementById("unix-time"));
+    });
 
+    function copyToClipboard(elem) {
+        // create hidden text element, if it doesn't already exist
+        var targetId = "_hiddenCopyText_";
+        var isInput = elem.tagName === "h5";
+        var origSelectionStart, origSelectionEnd;
+        if (isInput) {
+            // can just use the original source element for the selection and copy
+            target = elem;
+            origSelectionStart = elem.selectionStart;
+            origSelectionEnd = elem.selectionEnd;
+        } else {
+            // must use a temporary form element for the selection and copy
+            target = document.getElementById(targetId);
+            if (!target) {
+                var target = document.createElement("textarea");
+                target.style.position = "absolute";
+                target.style.left = "-9999px";
+                target.style.top = "0";
+                target.id = targetId;
+                document.body.appendChild(target);
+            }
+            target.textContent = elem.textContent;
+        }
+        // select the content
+        var currentFocus = document.activeElement;
+        target.focus();
+        target.setSelectionRange(0, target.value.length);
+
+        // copy the selection
+        var succeed;
+        try {
+            succeed = document.execCommand("copy");
+        } catch(e) {
+            succeed = false;
+        }
+        // restore original focus
+        if (currentFocus && typeof currentFocus.focus === "function") {
+            currentFocus.focus();
+        }
+
+        if (isInput) {
+            // restore prior selection
+            elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+        } else {
+            // clear temporary content
+            target.textContent = "";
+        }
+        alert('Unix время скопировано!')
+        return succeed;
+    }
 
   /*
     =======================================================================
